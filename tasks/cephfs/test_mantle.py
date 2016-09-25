a89d5f8dd745675f74cefc9e8c427e56f5125aa3
Michael Sevilla <mikesevilla3@gmail.com>
2016-09-24 22:50:18 -0700

    shows the blocking rados pull

--- tasks/cephfs/test_mantle.py
+++ tasks/cephfs/test_mantle.py
@@ -1,4 +1,7 @@
 from tasks.cephfs.cephfs_test_case import CephFSTestCase
+import json
+#import tasks.ceph_manager
+#from tasks.teuthology import misc as teuthology
 import logging
 
 log = logging.getLogger(__name__)
@@ -24,64 +27,97 @@ class TestMantle(CephFSTestCase):
         with self.assert_cluster_log(failure + obj + " " + expect):
             log.info("run a " + obj + " balancer that expects=" + expect)
 
-    def test_version_empty(self):
-        self.start_mantle()
-        expect = " : (2) No such file or directory"
+    #def test_version_empty(self):
+    #    self.start_mantle()
+    #    expect = " : (2) No such file or directory"
 
-        self.fs.mon_manager.raw_cluster_cmd_result('mds', 'set', 'balancer')
-        with self.assert_cluster_log(failure + expect): pass
+    #    self.fs.mon_manager.raw_cluster_cmd_result('mds', 'set', 'balancer')
+    #    with self.assert_cluster_log(failure + expect): pass
 
-        self.fs.mon_manager.raw_cluster_cmd_result('mds', 'set', 'balancer', " ")
-        with self.assert_cluster_log(failure + " " + expect): pass
+    #    self.fs.mon_manager.raw_cluster_cmd_result('mds', 'set', 'balancer', " ")
+    #    with self.assert_cluster_log(failure + " " + expect): pass
 
-    def test_version_not_in_rados(self):
-        self.start_mantle()
-        expect = failure + "ghost.lua : (2) No such file or directory"
-        self.fs.mon_manager.raw_cluster_cmd_result('mds', 'set', 'balancer', "ghost.lua")
-        with self.assert_cluster_log(expect): pass
+    #def test_version_not_in_rados(self):
+    #    self.start_mantle()
+    #    expect = failure + "ghost.lua : (2) No such file or directory"
+    #    self.fs.mon_manager.raw_cluster_cmd_result('mds', 'set', 'balancer', "ghost.lua")
+    #    with self.assert_cluster_log(expect): pass
 
-    def test_balancer_invalid(self):
-        self.start_mantle()
-        expect = ": (22) Invalid argument"
+    #def test_balancer_invalid(self):
+    #    self.start_mantle()
+    #    expect = ": (22) Invalid argument"
 
-        lua_code = "this is invalid lua code!"
-        self.push_balancer("invalid.lua", lua_code, expect)
+    #    lua_code = "this is invalid lua code!"
+    #    self.push_balancer("invalid.lua", lua_code, expect)
 
-        lua_code = "BAL_LOG()"
-        self.push_balancer("invalid_log.lua", lua_code, expect)
+    #    lua_code = "BAL_LOG()"
+    #    self.push_balancer("invalid_log.lua", lua_code, expect)
 
-        lua_code = "BAL_LOG(0)"
-        self.push_balancer("invalid_log_again.lua", lua_code, expect)
+    #    lua_code = "BAL_LOG(0)"
+    #    self.push_balancer("invalid_log_again.lua", lua_code, expect)
 
-    def test_balancer_valid(self):
-        self.start_mantle()
-        lua_code = "BAL_LOG(0, \"test\")\nreturn {3, 4}"
-        self.fs.mon_manager.raw_cluster_cmd_result('mds', 'set', 'balancer', "valid.lua")
-        self.fs.rados(["put", "valid.lua", "-"], stdin_data=lua_code)
-        with self.assert_cluster_log(success + "valid.lua"):
-            log.info("run a valid.lua balancer")
+    #def test_balancer_valid(self):
+    #    self.start_mantle()
+    #    lua_code = "BAL_LOG(0, \"test\")\nreturn {3, 4}"
+    #    self.fs.mon_manager.raw_cluster_cmd_result('mds', 'set', 'balancer', "valid.lua")
+    #    self.fs.rados(["put", "valid.lua", "-"], stdin_data=lua_code)
+    #    with self.assert_cluster_log(success + "valid.lua"):
+    #        log.info("run a valid.lua balancer")
 
-    def test_return_invalid(self):
-        self.start_mantle()
-        expect = ": (22) Invalid argument"
+    #def test_return_invalid(self):
+    #    self.start_mantle()
+    #    expect = ": (22) Invalid argument"
 
-        lua_code = "return \"hello\""
-	self.push_balancer("string.lua", lua_code, expect)
+    #    lua_code = "return \"hello\""
+    #    self.push_balancer("string.lua", lua_code, expect)
 
-        lua_code = "return 3"
-	self.push_balancer("number.lua", lua_code, expect)
+    #    lua_code = "return 3"
+    #    self.push_balancer("number.lua", lua_code, expect)
 
-        lua_code = "return {}"
-	self.push_balancer("dict_empty.lua", lua_code, expect)
+    #    lua_code = "return {}"
+    #    self.push_balancer("dict_empty.lua", lua_code, expect)
 
-        lua_code = "return {\"this\", \"is\", \"a\", \"test\"}"
-	self.push_balancer("dict_of_strings.lua", lua_code, expect)
+    #    lua_code = "return {\"this\", \"is\", \"a\", \"test\"}"
+    #    self.push_balancer("dict_of_strings.lua", lua_code, expect)
 
-        lua_code = "return {3, \"test\"}"
-	self.push_balancer("dict_of_mixed.lua", lua_code, expect)
+    #    lua_code = "return {3, \"test\"}"
+    #    self.push_balancer("dict_of_mixed.lua", lua_code, expect)
 
-        lua_code = "return {3}"
-	self.push_balancer("not_enough_numbers.lua", lua_code, expect)
+    #    lua_code = "return {3}"
+    #    self.push_balancer("not_enough_numbers.lua", lua_code, expect)
 
-        lua_code = "return {3, 4, 5, 6, 7, 8, 9}"
-	self.push_balancer("too_many_numbers.lua", lua_code, expect)
+    #    lua_code = "return {3, 4, 5, 6, 7, 8, 9}"
+    #    self.push_balancer("too_many_numbers.lua", lua_code, expect)
+
+    def test_dead_osd(self):
+         #self.fs.mon_manager.raw_cluster_cmd_result(['osd', ])
+         #x = self.fs.mon_manager.raw_cluster_cmd_result('osd')
+         self.start_mantle()
+
+         osd_map = json.loads(self.fs.mon_manager.raw_cluster_cmd('osd', 'dump', '--format=json-pretty'))
+         osd_count = len(osd_map['osds'])
+         print "OSD_COUNT", osd_count
+         print "OSD_MAP", json.dumps(osd_map, indent=4, sort_keys=True)
+         for i in range(0, osd_count):
+           print "\t KILLING OSD", str(i)
+           self.fs.mon_manager.raw_cluster_cmd_result('osd', 'down', str(i))
+           self.fs.mon_manager.raw_cluster_cmd_result('osd', 'out', str(i))
+         print "OSD_MAP", json.dumps(osd_map, indent=4, sort_keys=True)
+
+         # put the balancer in there, so it registers... balance a couple times,
+         # then kill the OSDs so that it knows its there but blocks and dies
+         #lua_code = "BAL_LOG(0, \"test\")\nreturn {3, 4}"
+         self.fs.mon_manager.raw_cluster_cmd_result('mds', 'set', 'balancer', "valid.lua")
+         #self.fs.rados(["put", "valid.lua", "-"], stdin_data=lua_code)
+         #with self.assert_cluster_log(success + "valid.lua"):
+         #    log.info("run a valid.lua balancer")
+
+         #manager = tasks.ceph_manager.CephManager(
+         #    mon,
+         #    ctx=ctx,
+         #    logger=log.getChild('ceph_manager'),
+         #    )
+         #first_mon = teuthology.get_first_mon(ctx, config)
+         #(mon,) = self.ctx.cluster.only(first_mon).remotes.iterkeys()
+
+
